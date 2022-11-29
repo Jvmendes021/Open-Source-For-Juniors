2cacf947dc88c30c3955b1eab8991f1bbb7bb345
jvmendes021 <joao.vitor13mendes@gmail.com>
2022-11-29 01:17:58 -0300

    Project completed successfully!

---
+++ pages/index.tsx
@@ -0,0 +1,37 @@
+import Head from 'next/head';
+import { PostCard, Categories, PostWidget } from '../components';
+import { getFeaturedPosts, getPosts } from '../services';
+import { FeaturedPosts } from '../sections';
+
+
+export default function Home ({ posts }) {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <Head>
+        <title> Open source for Juniors</title>
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
+      <FeaturedPosts />
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+        <div className='lg:col-span-8 col-span-1'>
+          {posts.map((post) => <PostCard post={post.node} key={post.title} />)}
+        </div>
+        <div className='lg:col-span-4 col-span-1'>
+          <div className='lg:sticky relative top-8'>
+            <PostWidget categories={undefined} slug={undefined} />
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+//Buscar dados no tempo de compilação
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts }
+  };
+}
+
